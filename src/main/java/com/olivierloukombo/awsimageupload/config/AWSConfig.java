e72e7c877e2259e2ee4c3bd59af67f4a43caf8b6
Olivier Loukombo <olivierloukombo@gmail.com>
2022-06-14 16:21:01 +0100

    add AWS config

---
+++ src/main/java/com/olivierloukombo/awsimageupload/config/AWSConfig.java
@@ -0,0 +1,27 @@
+package com.olivierloukombo.awsimageupload.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSConfig {
+    @Bean
+    public AmazonS3 s3(){
+        Dotenv dotenv = Dotenv.load();
+        String accesskey = dotenv.get("ACCESS_KEY");
+        String secretkey = dotenv.get("SECRET_KEY");
+        AWSCredentials awsCredentials = new BasicAWSCredentials(accesskey, secretkey);
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+
+    }
+}
+
